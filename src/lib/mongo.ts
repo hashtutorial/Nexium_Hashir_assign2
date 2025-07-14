ac73fca2d7ff13ce3c7b266f4fb45bbfbe30b3c3
hashtutorial <l227753@lhr.nu.edu.pk>
2025-07-14 22:14:05 +0500

    Update mongo.ts to resolve deployment issues

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+declare global {
+  var mongoose: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
@@ -16,8 +23,11 @@ export async function connectToMongo() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
